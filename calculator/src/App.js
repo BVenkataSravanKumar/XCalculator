192ed950b72b13aff1baefda6f55f1e28b806bb2
VENKAT <131847253+BVENKATASRAVANAKUMAR@users.noreply.github.com>
2025-08-26 01:41:36 +0530

    inital commit

---
+++ calculator/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [expression, setExpression] = useState("");
+  const [result, setResult] = useState("");      
+  const handleClick = (value) => {
+    if (value === "C") {
+      setExpression("");
+      setResult("");
+    } else if (value === "Enter") {
+      try {
+        if (!/^[0-9+\-*/.]+$/.test(expression)) {
+          setResult("Error");
+          return;
+        }
+            // eslint-disable-next-line
+        const res = Function("return " + expression)(); 
+        setResult(res.toString());
+      } catch {
+        setResult("Error");
+      }
+    } else {
+      setExpression(expression + value);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>React Calculator</h1>
+      <input type="text" value={expression} readOnly />
+      <h2>{result}</h2>
+
+      <div className="calculator">
+        {["7", "8", "9", "+",
+          "4", "5", "6", "-",
+          "1", "2", "3", "*",
+          "C", "0", "Enter", "/"].map((btn) => (
+          <button key={btn} onClick={() => handleClick(btn)}>
+            {btn}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
